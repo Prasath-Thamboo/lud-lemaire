fa6d8588663ba82b506650452cd6ce19e638a9e4
Prasath.Thamboo <prasath1@hotmail.fr>
2025-02-28 14:46:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,139 @@
+//-------------------------------------------------------------------Navbar----------------------------------------------------------------------------//
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn .fa-bars').toggleClass("active");
+    });
+
+
+//-------------------------------------------------------------------Typing----------------------------------------------------------------------------//
+
+
+
+    //strings: ["Conseils pour PME", "Gestion de projet", "Installation d'outils modernes"]
+    // typing text animation script
+    var typed = new Typed(".typing", {
+      strings : ["Advice for SMEs", "Project management", "Installation of modern tools"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+});
+
+// *****************************************Animation focus de contact ******************************************* //
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+// *****************************************Section article ******************************************* //
+
+
+
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabContents = document.querySelectorAll('.content');
+const faqs = document.querySelectorAll('.tab-button')
+
+
+// Ajouter un événement de clic à chaque bouton
+tabButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Supprimer la classe active de tous les boutons et contenus
+    tabButtons.forEach(btn => btn.classList.remove('active'));
+    tabContents.forEach(content => content.classList.remove('active'));
+
+    // Ajouter la classe active au bouton cliqué et à son contenu correspondant
+    button.classList.add('active');
+    const tabId = button.getAttribute('data-tab');
+    document.getElementById(`tab-${tabId}`).classList.add('active');
+
+    faqs.forEach
+  });
+});
+
+
+
+
+// *********************************************Slider de logo***********************************************//
+
+
+
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
+
+
+
+// *****************************************Cookie Banner ******************************************* //
+
+document.addEventListener("DOMContentLoaded", function(){
+  const cookieBanner = document.getElementById("cookie-banner")
+  const acceptButton = document.getElementById("accept-cookies")
+
+  function executeScript(){
+      console.log("Déclenchement")
+  }
+
+  if(localStorage.getItem('cookiesAccepted') === 'true'){
+      cookieBanner.style.display = "none"
+      executeScript()
+  }
+
+  acceptButton.addEventListener('click', function(){
+      localStorage.setItem('cookiesAccepted', 'true')
+      cookieBanner.style.display = "none"
+      executeScript()
+  })
+})
+
+
+
+// *****************************************Fin ******************************************* //
+
---
+++ trad.js
@@ -0,0 +1,211 @@
+const translations = {
+    en: {
+        homee:"Home",
+        about:"About",
+        news:"News",
+        homeee:"Home",
+        aboute:"About",
+        newss:"News", 
+        welcome:"Welcome to the website of",
+        expert:"We are expert in",
+        contacten:"Contact us", 
+        valtitle :"Our values",
+        val1tit:"Business advisory",
+        val1tex:"We provide consulting services in project and management that fulfill your company's needs",
+        val2tit:"Management tools",
+        val2tex:"We help you manage your projects through cutting edge technologies such as Power Platfom and Dynamics 365",
+        val3tit:"Trainings",
+        val3tex:"We train and support your teams using our own methodologies",
+        arttitle:"Our important articles",
+        arttit1:"Optimize your business management with Microsoft Business Central",
+        arttit2:"Choose Dynamics 365 for customer relationship management (CRM)",
+        arttit3:"Enhance your processes with Power Platform",
+        arttit4:"Improve your performance by going digital",
+        arttit5:"Use the power of data analysis with Power BI",
+        arttit6:"Integrate Microsoft 365 seamlessly with your business systems",
+        arttex1:"Discover how Microsoft Business Central can simplify your business management by centralizing all your processes in a single platform. This solution enables you to automate tasks, improve financial visibility and make decisions based on real-time data.",
+        arttex2:"Choosing Dynamics 365 for customer relationship management (CRM) means opting for a complete solution that unifies sales, marketing and customer service. Gain a 360° view of your customers, automate tasks, personalize interactions and make decisions based on real-time data to improve engagement and loyalty.",
+        arttex3:"Enhance your processes with Power Platform, a suite of Microsoft tools for creating customized applications, automating repetitive tasks and analyzing data. Easily connect your different information sources, optimize your workflows and gain in efficiency to accelerate your company's digital transformation.",
+        arttex4:"Digital transformation improves your business performance by automating tasks, optimizing processes and facilitating access to real-time data. It enables you to better understand customer needs, accelerate decision-making and personalize offers, all of which increase efficiency and boost your company's growth.",
+        arttex5:"Discover the power of data analysis with Power BI, a Microsoft tool that transforms your data into actionable information. Create interactive dashboards, visualize trends in real time and make informed decisions to boost your performance. Simplify complex analysis and easily share insights with your team.",
+        arttex6:"Microsoft 365 integration with your business systems delivers a seamless experience, connecting your productivity tools to your business applications. Easily access data, collaborate in real time and automate workflows. This integration boosts efficiency, reduces silos and optimizes your operations.",
+        arttitlem:"Our important articles",
+        arttit1m:"Optimize your business management with Microsoft Business Central",
+        arttit2m:"Choose Dynamics 365 for customer relationship management (CRM)",
+        arttit3m:"Enhance your processes with Power Platform",
+        arttit4m:"Improve your performance by going digital",
+        arttit5m:"Use the power of data analysis with Power BI",
+        arttit6m:"Integrate Microsoft 365 seamlessly with your business systems",
+        arttex1m:"Discover how Microsoft Business Central can simplify your business management by centralizing all your processes in a single platform. This solution enables you to automate tasks, improve financial visibility and make decisions based on real-time data.",
+        arttex2m:"Choosing Dynamics 365 for customer relationship management (CRM) means opting for a complete solution that unifies sales, marketing and customer service. Gain a 360° view of your customers, automate tasks, personalize interactions and make decisions based on real-time data to improve engagement and loyalty.",
+        arttex3m:"Enhance your processes with Power Platform, a suite of Microsoft tools for creating customized applications, automating repetitive tasks and analyzing data. Easily connect your different information sources, optimize your workflows and gain in efficiency to accelerate your company's digital transformation.",
+        arttex4m:"Digital transformation improves your business performance by automating tasks, optimizing processes and facilitating access to real-time data. It enables you to better understand customer needs, accelerate decision-making and personalize offers, all of which increase efficiency and boost your company's growth.",
+        arttex5m:"Discover the power of data analysis with Power BI, a Microsoft tool that transforms your data into actionable information. Create interactive dashboards, visualize trends in real time and make informed decisions to boost your performance. Simplify complex analysis and easily share insights with your team.",
+        arttex6m:"Microsoft 365 integration with your business systems delivers a seamless experience, connecting your productivity tools to your business applications. Easily access data, collaborate in real time and automate workflows. This integration boosts efficiency, reduces silos and optimizes your operations.",
+        servtitle:"Our Services",
+        valtitle2:"What Navilone can offer you",
+        servtitle1:"Needs Analysis",
+        servtitle2:"Tools required according to your needs",
+        servtitle3:"Implementation",
+        servtitle4:"Users training",
+        servp1:"Understand your field of activity, foresee opportunities for its development.",
+        servp2:"After a thorough analysis, we'll be able to offer you the best solutions.",
+        servp3:"The installation of modern tools to make it easier for you to manage every movement in your company.",
+        servp4:"All people who will be required to use these tools will be trained.",
+        contitle:"Talk to us",
+        conp1:"Leave us your information and we will contact you back as soon as possible",
+        conp2:"*The collected information from this form will be carefully handled to answer your needs",
+        conp3:"Contact us also in here :",
+        conp4:"Name",
+        conp5:"Submit",
+        footitle1:"Navilone",
+        footitle2:"Important information",
+        footitle3:"All rights reserved",
+        foo1:"Turn your ideas into successes with expert, tailor-made, results-oriented project management.",
+        foo2:"Terms of use",
+        foo3:"Privacy policy",
+
+
+    },    
+    
+    fr: { 
+        homee:"Accueil",
+        about: "A propos",
+        news:"Articles",
+        homeee:"Accueil",
+        aboute: "A propos",
+        newss:"Articles",
+        welcome:"Bienvenue sur le site de",
+        expert:"Nous sommes experts en",
+        contacten:"Contactez-nous",
+        valtitle :"Nos valeurs",
+        val1tit:"Conseil pour PME",
+        val1tex:"Nous proposons des services de conseil en gestion de projet qui répondent aux besoins spécifiques de votre entreprise.",
+        val2tit:"Outils de gestion",
+        val2tex:"Nous mettons en place les outils nécessaires pour la gestion de vos projets avec Powerplateform et Dynamics365.",
+        val3tit:"Formations",
+        val3tex:"Nous formons et suivons les collaborateurs pour utiliser les outils de manière optimales que nous vous avons mis en place.",
+        arttitle:"Nos articles importants",
+        arttit1:"Optimisez votre gestion d'entreprise avec Microsoft Business Central",
+        arttit2:"Choisissez Dynamics 365 pour la gestion de relation client (CRM)",
+        arttit3:"Améliorez vos processus grâce à Power Platform",
+        arttit4:"Améliorez vos performances en la digitalisant ",
+        arttit5:"Utilisez la puissance de l’analyse de données avec Power BI",
+        arttit6:"Intégrez Microsoft 365 de manière transparente à vos systèmes d'entreprise",
+        arttex1:" Découvrez comment Microsoft Business Central peut simplifier la gestion de votre entreprise en centralisant tous vos processus en une seule plateforme. Cette solution permet d'automatiser les tâches, d'améliorer la visibilité financière et de prendre des décisions basées sur des données en temps réel.",
+        arttex2:"Choisir Dynamics 365 pour la gestion de la relation client (CRM), c'est opter pour une solution complète qui unifie les ventes, le marketing et le service client. Bénéficiez d'une vue à 360° sur vos clients, automatisez les tâches, personnalisez les interactions et prenez des décisions basées sur des données en temps réel pour améliorer l'engagement et la fidélisation.",
+        arttex3:"Améliorez vos processus avec Power Platform, une suite d'outils Microsoft qui permet de créer des applications personnalisées, d'automatiser les tâches répétitives et d'analyser les données. Connectez facilement vos différentes sources d'information, optimisez vos flux de travail et gagnez en efficacité pour accélérer la transformation digitale de votre entreprise.",
+        arttex4:"La transformation digitale améliore vos performances commerciales en automatisant les tâches, optimisant les processus et facilitant l'accès aux données en temps réel. Elle permet de mieux comprendre les besoins des clients, d'accélérer la prise de décision et de personnaliser les offres, ce qui augmente l'efficacité et booste la croissance de votre entreprise.",
+        arttex5:"Découvrez la puissance de l'analyse de données avec Power BI, un outil Microsoft qui transforme vos données en informations exploitables. Créez des tableaux de bord interactifs, visualisez les tendances en temps réel et prenez des décisions éclairées pour booster vos performances. Simplifiez l'analyse complexe et partagez facilement les insights avec votre équipe.",
+        arttex6:"L'intégration de Microsoft 365 avec vos systèmes d'entreprise offre une expérience fluide, connectant vos outils de productivité à vos applications métier. Accédez facilement aux données, collaborez en temps réel et automatisez les flux de travail. Cette intégration renforce l'efficacité, réduit les silos et optimise vos opérations.",
+        arttitlem:"Nos articles importants",
+        arttit1m:"Optimisez votre gestion d'entreprise avec Microsoft Business Central",
+        arttit2m:"Choisissez Dynamics 365 pour la gestion de relation client (CRM)",
+        arttit3m:"Améliorez vos processus grâce à Power Platform",
+        arttit4m:"Améliorez vos performances en la digitalisant ",
+        arttit5m:"Utilisez la puissance de l’analyse de données avec Power BI",
+        arttit6m:"Intégrez Microsoft 365 de manière transparente à vos systèmes d'entreprise",
+        arttex1m:"Découvrez comment Microsoft Business Central peut simplifier la gestion de votre entreprise en centralisant tous vos processus en une seule plateforme. Cette solution permet d'automatiser les tâches, d'améliorer la visibilité financière et de prendre des décisions basées sur des données en temps réel.",
+        arttex2m:"Choisir Dynamics 365 pour la gestion de la relation client (CRM), c'est opter pour une solution complète qui unifie les ventes, le marketing et le service client. Bénéficiez d'une vue à 360° sur vos clients, automatisez les tâches, personnalisez les interactions et prenez des décisions basées sur des données en temps réel pour améliorer l'engagement et la fidélisation.",
+        arttex3m:"Améliorez vos processus avec Power Platform, une suite d'outils Microsoft qui permet de créer des applications personnalisées, d'automatiser les tâches répétitives et d'analyser les données. Connectez facilement vos différentes sources d'information, optimisez vos flux de travail et gagnez en efficacité pour accélérer la transformation digitale de votre entreprise.",
+        arttex4m:"La transformation digitale améliore vos performances commerciales en automatisant les tâches, optimisant les processus et facilitant l'accès aux données en temps réel. Elle permet de mieux comprendre les besoins des clients, d'accélérer la prise de décision et de personnaliser les offres, ce qui augmente l'efficacité et booste la croissance de votre entreprise.",
+        arttex5m:"Découvrez la puissance de l'analyse de données avec Power BI, un outil Microsoft qui transforme vos données en informations exploitables. Créez des tableaux de bord interactifs, visualisez les tendances en temps réel et prenez des décisions éclairées pour booster vos performances. Simplifiez l'analyse complexe et partagez facilement les insights avec votre équipe.",
+        arttex6m:"L'intégration de Microsoft 365 avec vos systèmes d'entreprise offre une expérience fluide, connectant vos outils de productivité à vos applications métier. Accédez facilement aux données, collaborez en temps réel et automatisez les flux de travail. Cette intégration renforce l'efficacité, réduit les silos et optimise vos opérations.",
+        servtitle:"Nos services",
+        valtitle2:"Ce que Navilone peut vous offrir",
+        servtitle1:"Analyse du besoin",
+        servtitle2:"Outils nécessaires selon les besoins",
+        servtitle3:"Mise en place",
+        servtitle4:"Formation des utilisateurs",
+        servp1:"Comprendre votre domaine d'activité, entrevoir les possibilités pour son développement.",
+        servp2:"Après une analyse pertinente nous serons à même de vous proposer les meilleures solutions.",
+        servp3:"L'installation d'outils modernes pour vous faciliter la gestion de tout mouvement dans votre entreprise.",
+        servp4:"Toutes les personnes qui seront amenées à utiliser ces outils seront formées.",
+        contitle:"Nous sommes à votre disposition",
+        conp1:"Laissez-nous vos coordonnées et nous vous contacterons au plus vite ",
+        conp2:"*Les informations recueillies à partir de ce formulaire font l'objet d'un traitement informatisé destiné à répondre à votre demande.",
+        conp3:"Contactez-nous ici aussi : ",
+        conp4:"Nom",
+        conp5:"Envoyer",
+        footitle1:"La société Navilone",
+        footitle2:"Informations importantes",
+        footitle3:"Tous droits réservés",
+        foo1:"Transformez vos idées en succès grâce à une gestion de projet experte, sur-mesure et orientée résultats.",
+        foo2:"Mentions légales",
+        foo3:"Politique de confidentialité",
+
+        
+
+    },
+  };
+  
+  function setLanguage(lang) {
+    document.getElementById('homee').innerText = translations[lang].homee;
+    document.getElementById('about').innerText = translations[lang].about;
+    document.getElementById('news').innerText = translations[lang].news;
+    document.getElementById('homeee').innerText = translations[lang].homeee;
+    document.getElementById('aboute').innerText = translations[lang].aboute;
+    document.getElementById('newss').innerText = translations[lang].newss;
+    document.getElementById('welcome').innerText = translations[lang].welcome;
+    document.getElementById('expert').innerText = translations[lang].expert;
+    document.getElementById('contacten').innerText = translations[lang].contacten;
+    document.getElementById('valtitle').innerText = translations[lang].valtitle;
+    document.getElementById('valtitle2').innerText = translations[lang].valtitle2;
+    document.getElementById('val1tit').innerText = translations[lang].val1tit;
+    document.getElementById('val1tex').innerText = translations[lang].val1tex;
+    document.getElementById('val2tit').innerText = translations[lang].val2tit;
+    document.getElementById('val2tex').innerText = translations[lang].val2tex;
+    document.getElementById('val3tit').innerText = translations[lang].val3tit;
+    document.getElementById('val3tex').innerText = translations[lang].val3tex;
+    document.getElementById('arttitle').innerText = translations[lang].arttitle;
+    document.getElementById('arttit1').innerText = translations[lang].arttit1;
+    document.getElementById('arttit2').innerText = translations[lang].arttit2;
+    document.getElementById('arttit3').innerText = translations[lang].arttit3;
+    document.getElementById('arttit4').innerText = translations[lang].arttit4;
+    document.getElementById('arttit5').innerText = translations[lang].arttit5;
+    document.getElementById('arttit6').innerText = translations[lang].arttit6;
+    document.getElementById('arttex1').innerText = translations[lang].arttex1;
+    document.getElementById('arttex2').innerText = translations[lang].arttex2;
+    document.getElementById('arttex3').innerText = translations[lang].arttex3;
+    document.getElementById('arttex4').innerText = translations[lang].arttex4;
+    document.getElementById('arttex5').innerText = translations[lang].arttex5;
+    document.getElementById('arttex6').innerText = translations[lang].arttex6;
+    document.getElementById('arttitlem').innerText = translations[lang].arttitle;
+    document.getElementById('arttit1m').innerText = translations[lang].arttit1m;
+    document.getElementById('arttit2m').innerText = translations[lang].arttit2m;
+    document.getElementById('arttit3m').innerText = translations[lang].arttit3m;
+    document.getElementById('arttit4m').innerText = translations[lang].arttit4m;
+    document.getElementById('arttit5m').innerText = translations[lang].arttit5m;
+    document.getElementById('arttit6m').innerText = translations[lang].arttit6m;
+    document.getElementById('arttex1m').innerText = translations[lang].arttex1m;
+    document.getElementById('arttex2m').innerText = translations[lang].arttex2m;
+    document.getElementById('arttex3m').innerText = translations[lang].arttex3m;
+    document.getElementById('arttex4m').innerText = translations[lang].arttex4m;
+    document.getElementById('arttex5m').innerText = translations[lang].arttex5m;
+    document.getElementById('arttex6m').innerText = translations[lang].arttex6m;
+    document.getElementById('servtitle').innerText = translations[lang].servtitle;
+    document.getElementById('servtitle2').innerText = translations[lang].servtitle2;
+    document.getElementById('servtitle1').innerText = translations[lang].servtitle1;
+    document.getElementById('servtitle3').innerText = translations[lang].servtitle3;
+    document.getElementById('servtitle4').innerText = translations[lang].servtitle4;
+    document.getElementById('servp1').innerText = translations[lang].servp1;
+    document.getElementById('servp2').innerText = translations[lang].servp2;
+    document.getElementById('servp3').innerText = translations[lang].servp3;
+    document.getElementById('servp4').innerText = translations[lang].servp4;
+    document.getElementById('contitle').innerText = translations[lang].contitle;
+    document.getElementById('conp1').innerText = translations[lang].conp1;
+    document.getElementById('conp2').innerText = translations[lang].conp2;
+    document.getElementById('conp3').innerText = translations[lang].conp3;
+    document.getElementById('conp4').innerText = translations[lang].conp4;
+    document.getElementById('conp5').innerText = translations[lang].conp5;
+    document.getElementById('footitle1').innerText = translations[lang].footitle1;
+    document.getElementById('footitle2').innerText = translations[lang].footitle2;
+    document.getElementById('footitle3').innerText = translations[lang].footitle3;
+    document.getElementById('foo1').innerText = translations[lang].foo1;
+    document.getElementById('foo2').innerText = translations[lang].foo2;
+    document.getElementById('foo3').innerText = translations[lang].foo3;
+
+  }
+  
+  // Langue par défaut
+  setLanguage('en');
